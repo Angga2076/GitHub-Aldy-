aeccdc07760463ee1fb1ffe9de9d7141bd2a661f
Angga2076 <rismanksayang@gmail.com>
2025-08-17 05:13:23 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/NAMA-REPO/",   // ganti NAMA-REPO dengan nama repo GitHub kamu
+  base: "/GitHub-Aldy/",   
 })
