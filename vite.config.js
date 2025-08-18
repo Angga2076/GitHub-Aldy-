8b723f0c97cca81d7069d6fff95a978df7e7e5fe
Angga2076 <rismanksayang@gmail.com>
2025-08-19 01:42:49 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Aldy02211/",
+  base: "/GitHub-Aldy/",
   plugins: [react(), tailwindcss()],
 })
