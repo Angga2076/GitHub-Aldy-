b353d7b5aa0355e38c54adedd76d20143d44fe9e
Angga2076 <rismanksayang@gmail.com>
2025-08-19 20:53:07 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// ✅ Tailwind v4 sudah pakai @tailwindcss/vite, jadi tidak perlu postcss.config.js
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
