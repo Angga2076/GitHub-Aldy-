d4bae8f69367507211611cb1b679680aa93e47ca
Angga2076 <rismanksayang@gmail.com>
2025-08-19 17:12:06 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // kalau kamu pakai React/JS di folder src
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
