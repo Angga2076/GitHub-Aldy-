13e439c5c7f4b702443fcfd5ceafd8341a8f5e98
Angga2076 <rismanksayang@gmail.com>
2025-08-19 20:48:03 +0800

    Delete src/components/Footer.jsx

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full py-6 mt-12 bg-gray-900 text-center text-gray-300">
-      <p>© {new Date().getFullYear()} My Portofolio. All rights reserved.</p>
-    </footer>
-  )
-}
